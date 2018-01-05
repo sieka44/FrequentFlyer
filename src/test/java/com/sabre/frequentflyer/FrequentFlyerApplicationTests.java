1e4a5e4966f3ffbe8222d2c0e63f2d3c31f26692
Zax37 <c37zax@gmail.com>
2018-01-05 20:51:34 +0100

    Added missing newline characters, fixed indents.

--- src/test/java/com/sabre/frequentflyer/FrequentFlyerApplicationTests.java
+++ src/test/java/com/sabre/frequentflyer/FrequentFlyerApplicationTests.java
@@ -8,9 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class FrequentFlyerApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
-
 }
