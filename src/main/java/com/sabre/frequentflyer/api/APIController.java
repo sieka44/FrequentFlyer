2ef655d6236e64b13f95ddd42e98db9925ca4170
sieka44 <sieka444@interia.pl>
2018-01-12 11:41:15 +0100

    API Clean-Up.

--- src/main/java/com/sabre/frequentflyer/api/APIController.java
+++ src/main/java/com/sabre/frequentflyer/api/APIController.java
@@ -8,8 +8,6 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
