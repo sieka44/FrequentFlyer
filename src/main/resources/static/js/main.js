598cf4ccb895d893ee1ea6f8225d08a5aa0a9078
Zax37 <c37zax@gmail.com>
2018-01-15 18:05:04 +0100

    Navigation bar. Home tab template.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,14 @@
+var loaded = 0, toLoad = 2;
+
+function loadStep() {
+    loaded++;
+    if (loaded >= toLoad) {
+        $("#app").attr('loaded', 'true');
+        $(".determinate").show("slide", { direction: "left" }, 1000);
+        $(".tabs > .tab:first-child > a").click();
+    }
+}
+
 var application = new Vue({
     el: '#app',
     data: {
@@ -6,12 +17,16 @@ var application = new Vue({
     mounted() {
         this.$http.get('/api/v1/getUserStatus').then(
             response => {
-                console.log(response);
                 application.text = response.body;
+                loadStep()
             },
             response => {
                 console.log("error");
             }
         )
     }
+});
+
+$(function() {
+    loadStep();
 });
--- src/test/java/com/sabre/frequentflyer/FrequentFlyerApplicationTests.java
+++ src/test/java/com/sabre/frequentflyer/FrequentFlyerApplicationTests.java
@@ -10,5 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class FrequentFlyerApplicationTests {
 	@Test
 	public void contextLoads() {
+
 	}
 }
