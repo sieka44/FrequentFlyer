06e8bb4a70f8c05bb503a0ec964cf46fac11d6e9
Zax37 <c37zax@gmail.com>
2018-01-17 15:50:25 +0100

    Auth0 login, logout and user metadata handling.

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,24 +1,28 @@
-var loaded = 0, toLoad = 2;
+let loaded = 0, toLoad = 2;
 
 function loadStep() {
     loaded++;
     if (loaded >= toLoad) {
-        $("#app").attr('loaded', 'true');
-        $(".determinate").show("slide", { direction: "left" }, 1000);
-        $(".tabs > .tab:first-child > a").click();
+        setTimeout(function () {
+            $("#app").attr('loaded', 'true');
+            $(".determinate").show("slide", { direction: "left" }, 1000);
+            $(".tabs > .tab:first-child > a").click();
+        }, 1000);
     }
 }
 
-var application = new Vue({
+application = new Vue({
     el: '#app',
     data: {
-        text: "Hello world!"
+        user: {
+            name:'', picture:''
+        }
     },
     mounted() {
         this.$http.get('/api/v1/getUserStatus').then(
             response => {
                 application.text = response.body;
-                loadStep()
+                loadStep();
             },
             response => {
                 console.log("error");
@@ -27,6 +31,64 @@ var application = new Vue({
     }
 });
 
-$(function() {
-    loadStep();
+var href = window.location.href;
+var token = href.substring(href.indexOf("=")+1,href.indexOf("&"));
+
+// Initializing our Auth0Lock
+var lock = new Auth0Lock(
+    '3a0iMYzlXuCq5uQJJdAjKekCdF_dWfZe',
+    'frequent-flyer.eu.auth0.com',
+    {
+        closable: false,
+        languageDictionary: {
+            title: "FrequentFlyer"
+        },
+        theme: {
+            logo: 'http://www.primclub.ru/images/824400_airport_512x512.png',
+            primaryColor: '#FF6262'
+        }
+    }
+);
+// Listening for the authenticated event
+lock.on("authenticated", function(authResult) {
+    // Use the token in authResult to getUserInfo() and save it to localStorage
+    lock.getUserInfo(authResult.accessToken, function(error, profile) {
+        if (error) {
+            // Handle error
+            return;
+        }
+
+        localStorage.setItem('accessToken', authResult.accessToken);
+        localStorage.setItem('profile', JSON.stringify(profile));
+
+        application.user = {
+            picture: profile.picture,
+            name: profile.name
+        };
+        application.$forceUpdate();
+        loadStep();
+    });
 });
+
+if (token.length != 32 && (token = localStorage.getItem('accessToken'),
+    !token || token.length != 32)) {
+    lock.show();
+} else {
+    document.getElementById('loading').style.display = null;
+    if (profile = JSON.parse(localStorage.getItem('profile'))) {
+        application.user = {
+            picture: profile.picture,
+            name: profile.name
+        };
+        application.$forceUpdate();
+    }
+    loadStep();
+}
+
+function logout () {
+    lock.logout();
+    localStorage.removeItem( 'accessToken' );
+    localStorage.removeItem( 'profile' );
+}
+
+$(".button-collapse").sideNav();
